d316dfb6fe30ced4581d559b28ed1047801c0587
mkronvold <mkronvold@dev2013.dev.e2open.com>
2023-09-27 21:19:22 +0000

    unlinked

--- babfunc.sh
+++ babfunc.sh
@@ -1,1 +1,138 @@
-/e2open/home/mkronvold/.bash.d/sync/.bin/babfunc.sh
+#--------------------------------------------------------------------
+mktmps () {
+  tmp=()           # array of tmp files, refer to them as ${tmp[0]} thru ${tmp[tmps]}
+                   # yes this always makes one extra tmp[0] as a spare intentionally
+                   # change i=1 to change behavior to 0 to tmps-1
+  for ((i=0; i<=tmps; i++));
+  do
+    tmpfile=$( mktemp /dev/shm/${tempkey}_${program}_tmp.XXXXXXXXXX )
+    tmp+=($tmpfile)
+  done
+}
+
+#--------------------------------------------------------------------
+cleanup () {
+     #  Delete temporary files, then optionally exit given status.
+     local status=${1:-'0'}
+#     rm -f ${tmp[@]}
+     [[ $(which shred) ]] && shred --remove ${tmp[@]} || (dd if=/dev/urandom of=${tmp[@]} bs=1M count=100 ; rm -f ${tmp[@]} )
+     [ $status = '-1' ] ||  exit $status      #  thus -1 prevents exit.
+}
+
+#--------------------------------------------------------------------
+debug () {
+     #  Message with DEBUG: to stderr.          Usage: debug "message"
+     [ $DEBUG ] && echo -e "\n !! DEBUG: $1 "  >&2
+}
+
+#--------------------------------------------------------------------
+warn () {
+     #  Message with basename to stderr.          Usage: warn "message"
+     echo -e "\n !!  ${program}: $1 "  >&2
+}
+
+#--------------------------------------------------------------------
+dryrun () {
+     # Message with DRYRUN: if $DRYRUN is set otherwise run it
+     # Usage: dryrun "command"
+     if [ $DRYRUN ]; then
+       echo -e "\n -- DRYRUN: $1 "  >&1
+     else
+       IFS=' ' read -r -a command_arr <<< "${1}"
+       "${command_arr[@]}"
+     fi
+}
+
+
+#--------------------------------------------------------------------
+die () {
+     #  Exit with status of most recent command or custom status, after
+     #  cleanup and warn.      Usage: command || die "message" [status]
+     local status=${2:-"$?"}
+     cleanup -1  &&   warn "$1"  &&  exit $status
+}
+
+#--------------------------------------------------------------------
+trap "die 'SIG disruption, but cleanup finished.' 114" 1 2 3 15
+#    Cleanup after INTERRUPT: 1=SIGHUP, 2=SIGINT, 3=SIGQUIT, 15=SIGTERM
+
+#--------------------------------------------------------------------
+#   Formats the specified number(s) according to the rules of the
+#   current locale in terms of digit grouping (thousands separators).
+# Usage:
+#    groupDigits num ...
+# Examples:
+#   groupDigits 1000 # -> '1,000'
+#   groupDigits 1000.5 # -> '1,000.5'
+#   (LC_ALL=lt_LT.UTF-8; groupDigits 1000,5) # -> '1 000,5'
+groupDigits() {
+  local decimalMark fractPart
+  decimalMark=$(printf "%.1f" 0); decimalMark=${decimalMark:1:1}
+  for num; do
+    fractPart=${num##*${decimalMark}}; [[ "$num" == "$fractPart" ]] && fractPart=''
+    printf "%'.${#fractPart}f\n" "$num" || return
+  done
+}
+
+#--------------------------------------------------------------------
+# Calculate using bc
+# usage:
+#     calculate {options} expression
+#               --scale=n  number of digits to return
+# requires: bc
+calculate()
+{
+    floatscale=1 #default
+    result=
+    expression=
+    while [[ $# -gt 0 ]] && [[ "$1" == "--"* ]] ;
+    do
+        opt=${1}
+        case "${opt}" in
+            "--" )
+                break 2;;
+            "--scale="* )
+                floatscale="${opt#*=}";;
+            *)
+            #   erm.  nothing here.
+            ;;
+        esac
+        shift
+    done
+    expression=$*
+    result=$(echo "scale=${floatscale}; ${expression}" | bc -q 2>/dev/null)
+    printf '%*.*f' 0 "${floatscale}" "${result}"
+}
+
+#--------------------------------------------------------------------
+# Draw a horizontal line the width of the terminal
+# requires: tput
+hr () { printf "%0$(tput cols)d" | tr 0 ${1:-=}; }
+
+#--------------------------------------------------------------------
+# csv output helper, see usage
+csv()
+{
+  if [ ! "${1}" ]; then
+    echo "Usage: csv [items] >> {file.csv}"
+    echo "   [items] will be protected against embedded commas"
+    echo "   example:"
+    echo "   # csv 1 \"2 3 4 5\" \"6-7?8\" \"9,10\""
+    echo "   1,2 3 4 5,6-7?8,\"9,10\""
+    return
+  fi
+  #
+  local items=("$@") # quote and escape as needed
+                     # datatracker.ietf.org/doc/html/rfc4180
+  for i in "${!items[@]}"; do
+    if [[ "${items[$i]}" =~ [,\"] ]]; then
+       items[$i]=\"$(echo -n "${items[$i]}" | sed s/\"/\"\"/g)\"
+    fi
+  done
+  (
+    IFS=,
+    echo "${items[*]}"
+  )
+}
+
+#--------------------------------------------------------------------
--- tmcpull.sh
+++ tmcpull.sh
@@ -1,1 +1,138 @@
-/e2open/home/mkronvold/.bash.d/sync/.bin/tmcpull.sh
+#!/bin/bash
+
+tempkey=9492       # set this to something unique to your scripts
+tmps=1             # number of tmp files needed
+DEBUG=
+
+############ don't mess with these ###########
+#
+program=${0##*/}   # similar to using basename
+confdir=$(dirname "$0")
+[ $workdir ] || workdir=$confdir
+[ -d $workdir ] || workdir=$confdir
+#[ -f $confdir/bab.conf ] && source $confdir/bab.conf || die "No config file found in default location $confdir/bab.conf" 10
+[ -f $confdir/babfunc.sh ] && source $confdir/babfunc.sh || die "$confdir/babfunc.sh not found" 11
+mktmps
+#
+###############################################
+
+# get config
+#[ $listdir ] || warn "No listdir found in config" 10
+#[ -d $listdir ] || listdir=$(pwd)
+
+CSVLOOK=cat
+
+while [[ $# -gt 0 ]] && [[ "$1" == "--"* ]] ;
+do
+  opt=${1}
+  case "${opt}" in
+    "--" )
+      break 2;;
+    "--dry" ) DRYRUN=1;;
+    "--dryrun" ) DRYRUN=1;;
+    "--reqs"|"--requirements" ) [ $(which csvlook) ] && echo "csvkit found." || die "csvkit not found.  Install with: sudo pip install csvkit";;
+    "--csv" ) MKCSV=1;;
+    "--look"|"--csvlook" ) CSVLOOK=$(which csvlook || echo cat);;
+    "--save="* )
+      SAVE=1
+      SAVEFILE="${opt#*=}";;
+    "--test" ) DRYRUN=1;;
+    "--debug" ) DEBUG=1 ;;
+    "--break" ) BREAKPOINT=1;;
+    "--tkr" ) work=listtkr ;;
+    "--tkrcount" ) work=counttkr ;;
+    "--tns" ) work=listtns ;;
+    "--clusterdetails"|"--clusters"|"--tkc" ) work=listtkc ;;
+    "--tkg"|"--mgmt"|"--management" ) work=listmgmt ;;
+    "--workspaces"|"--wks" ) work=listworkspaces ;;
+    "--ls="* )
+      work=ls
+      ls="${opt#*=}";;
+    *)
+    #   erm.  nothing here.
+    ;;
+  esac
+  shift
+done
+
+
+#main
+debug "tmps=$tmps"
+debug "tmp()=${tmp[@]}"
+debug "tmp[0]=${tmp[0]}"
+debug "tmp[1]=${tmp[1]}"
+[ $BREAKPOINT ] && die "DEBUG BREAKPOINT" 99
+
+case "$work" in
+  "listtkr" )
+    debug "attempting to list tkrs of all tkcs"
+    if [ $MKCSV ]; then
+      if [ $SAVE ]; then
+        echo "clustername,distribution" > $SAVEFILE-$(date +'%Y-%m-%d_%H-%M-%S').csv
+        tanzu mission-control cluster list -o json | jq -r '.clusters[] | "\(.fullName.name) \(.spec.tkgServiceVsphere.distribution.version)"' | awk -F+ '{print $1}' | csvsort -c 2 | sed -e 's/\ /,/g' >> $SAVEFILE-$(date +'%Y-%m-%d_%H-%M-%S').csv
+      else
+        ( echo "clustername,distribution" ;
+        tanzu mission-control cluster list -o json | jq -r '.clusters[] | "\(.fullName.name) \(.spec.tkgServiceVsphere.distribution.version)"' | awk -F+ '{print $1}' | sed -e 's/\ /,/g' ) | csvsort -c 2 | $CSVLOOK
+      fi
+    else
+      tanzu mission-control cluster list -o json | jq -r '.clusters[] | "\(.fullName.name) \(.spec.tkgServiceVsphere.distribution.version)"' | awk -F+ '{print $1}'
+    fi
+    ;;
+  "counttkr" )
+    debug "attempting to count tkrs of all tkcs"
+    tanzu mission-control cluster list -o json | jq -r '.clusters[] | "\(.fullName.name) \(.spec.tkgServiceVsphere.distribution.version)"' | awk -F+ '{print $1}' | sed -e 's/\ /,/g' | csvstat -c 2 --freq --freq-count 25 | jq | sort | grep :
+    ;;
+  "listtkc" )
+    debug "attempting to list tkc details"
+    if [ $MKCSV ]; then
+      if [ $SAVE ]; then
+        echo "clustername,memorytotal,memorypercent,cputotal,cpupercent,nodecount,health,message" > $SAVEFILE-$(date +'%Y-%m-%d_%H-%M-%S').csv
+        tanzu mission-control cluster list -o json | jq -r '.clusters[] | "\(.fullName.name),\(.status.allocatedMemory.allocatable),\(.status.allocatedMemory.allocatedPercentage),\(.status.allocatedCpu.allocatable),\(.status.allocatedCpu.allocatedPercentage),\(.status.nodeCount),\(.status.health),\(.status.healthDetails.message)"' >> $SAVEFILE-$(date +'%Y-%m-%d_%H-%M-%S').csv
+      else
+        ( echo "clustername,memorytotal,memorypercent,cputotal,cpupercent,nodecount,health,message" ;
+        tanzu mission-control cluster list -o json | jq -r '.clusters[] | "\(.fullName.name),\(.status.allocatedMemory.allocatable),\(.status.allocatedMemory.allocatedPercentage),\(.status.allocatedCpu.allocatable),\(.status.allocatedCpu.allocatedPercentage),\(.status.nodeCount),\(.status.health),\(.status.healthDetails.message)"' ) | $CSVLOOK 2> /dev/null
+      fi
+    else
+      tanzu mission-control cluster list -o json | jq -r '.clusters[] | "\(.fullName.name) \(.status.allocatedMemory.allocatable) \(.status.allocatedMemory.allocatedPercentage) \(.status.allocatedCpu.allocatable) \(.status.allocatedCpu.allocatedPercentage) \(.status.nodeCount) \(.status.health) \(.status.healthDetails.message)"'
+    fi
+    ;;
+  "listtns" )
+    debug "attempting to list all tns by tkc"
+    if [ $MKCSV ]; then
+      if [ $SAVE ]; then
+        ( echo "namespace,clustername,workspace,mgmtcluster" ;
+        tanzu mission-control cluster namespace list -o json | jq -r '.namespaces[] | "\(.fullName.name),\(.fullName.clusterName),\(.spec.workspaceName),\(.fullName.managementClusterName)"' ) | csvsort -c 4,2,3,1 > $SAVEFILE-$(date +'%Y-%m-%d_%H-%M-%S').csv
+      else
+        ( echo "namespace,clustername,workspace,mgmtcluster" ;
+        tanzu mission-control cluster namespace list -o json | jq -r '.namespaces[] | "\(.fullName.name),\(.fullName.clusterName),\(.spec.workspaceName),\(.fullName.managementClusterName)"' ) | csvsort -c 4,2,3,1 | $CSVLOOK 2> /dev/null
+      fi
+    else
+      ( echo "namespace,clustername,workspace,mgmtcluster" ;
+      tanzu mission-control cluster namespace list -o json | jq -r '.namespaces[] | "\(.fullName.name),\(.fullName.clusterName),\(.spec.workspaceName),\(.fullName.managementClusterName)"' ) | csvsort -c 4,2,3,1
+    fi
+    ;;
+  "listmgmt" )
+    debug "attempting to list all mgmt-clusters"
+    debug "attempting to list all tkcs by tns"
+    if [ $MKCSV ]; then
+      if [ $SAVE ]; then
+        echo "fullName,ClusterGroup,health,READYmessage" > $SAVEFILE-$(date +'%Y-%m-%d_%H-%M-%S').csv
+        tanzu mission-control management-cluster list -o json | jq -r '.managementClusters[] | "\(.fullName.name),\(.spec.defaultClusterGroup),\(.status.health),\(.status.conditions.READY.message)"' | grep connected >> $SAVEFILE-$(date +'%Y-%m-%d_%H-%M-%S').csv
+      else
+        ( echo "fullName,ClusterGroup,health,READYmessage" ;
+        tanzu mission-control management-cluster list -o json | jq -r '.managementClusters[] | "\(.fullName.name),\(.spec.defaultClusterGroup),\(.status.health),\(.status.conditions.READY.message)"' | grep connected ) | $CSVLOOK 2> /dev/null
+      fi
+    else
+      tanzu mission-control management-cluster list
+    fi
+    ;;
+  "listworkspaces" )
+    debug "attempting to list all workspaces"
+    [ $DRYRUN ] && echo "tanzu mission-control workspace list -o json | jq '.workspaces[].fullName.name'" || tanzu mission-control workspace list -o json | jq '.workspaces[].fullName.name'
+    ;;
+  *)
+    die "nothing else to do" 100
+    ;;
+esac
+
+cleanup
